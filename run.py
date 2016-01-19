bc1c3bbc10865fe83e35a459753e2dfa50a7ad59
Maurilio Zeni <Maurilio Zeni>
2016-01-19 15:25:00 +0100

    first commit of the project

---
+++ run.py
@@ -0,0 +1,55 @@
+import sys
+import time
+import random
+import datetime
+import telepot
+import time
+
+prodB1 =4442.7
+prodB2 = 4290.2
+
+def sticazzi(chat_id):
+	day = time.strftime("%d").lstrip('0')
+	month = time.strftime("%m").lstrip('0')
+	file_name = month + " giorno " + day + "_" + month + " medie.txt"
+
+	fileHandle = open (file_name,"r" )
+	lineList = fileHandle.readlines()
+	fileHandle.close()
+
+	str = lineList[-2]
+	str = lineList[-1]
+	str = str.replace('   ','z')
+	str = str.replace(',','.')
+	str = str.split('z')
+	if (len(str)<8):
+		str = lineList[-2]
+		str = str.replace('   ','z')
+		str = str.replace(',','.')
+		str = str.split('z')
+
+	print(str)
+	result = float((float(float(prodB1) - float(str[6]))/float(float(prodB2) - float(str[7]))-1)*100)
+	print("%.2f%%" % result)
+	bot.sendMessage(chat_id, "Ultimo aggiornamento: " + str[0] + " alle ore " + str[1])
+	bot.sendMessage(chat_id, "Confronto produzioni odierne: " + "%.2f" % result + "%")
+
+def handle(msg):
+    chat_id = msg['chat']['id']
+    command = msg['text']
+
+    print('Got command: %s' % command)
+
+    if command == '/roll':
+        bot.sendMessage(chat_id, random.randint(1,6))
+    elif command == '/time':
+        bot.sendMessage(chat_id, str(datetime.datetime.now()))
+    elif command == '/prod':
+        sticazzi(chat_id)
+
+bot = telepot.Bot('137481181:AAGGl_Sqd86Qs2TT4W5xTTgjaGoZxrjxlVI')
+bot.notifyOnMessage(handle)
+print('I am listening ...')
+
+while 1:
+    time.sleep(10)

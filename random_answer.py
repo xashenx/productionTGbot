369958f88310f2fe4bede11e310e6a57d82d6df3
Fabrizio Zeni <fabrizio.zeni@gmail.com>
2016-02-16 10:59:14 +0100

    change % for jokes and added one

--- random_answer.py
+++ random_answer.py
@@ -8,17 +8,18 @@ jokes = ['Mi spiace non posso darti i dati!',
 'Secondo te ho già i dati? Può essere.',
 'Grazie per aver richiesto i dati. Ciao.',
 'Io sto bene, tu?',
-'Dici che c\'è il sole oggi?']
+'Dici che c\'è il sole oggi?',
+'E il cielo è sempre più bluuuuuu']
 
 
 def answer():
     random = randint(0, 9)
-    if random > 3:
+    if random > 2:
         return True
     else:
         return False
 
 
 def joke():
-    chosen = randint(0, 5)
+    chosen = randint(0, 7)
     return jokes[chosen]
